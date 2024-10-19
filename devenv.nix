35051fdb2140f65788e759ed3ca7a657f133e0a1
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-19 16:22:22 +0100

    feat: init Nix development environment

---
+++ devenv.nix
@@ -0,0 +1,44 @@
+{ lib, pkgs, ... }:
+{
+  env.GREET = ''
+    =============
+      Zero2Prod
+    =============
+  '';
+
+  packages = with pkgs; [
+    bacon
+    cargo-audit
+    cargo-tarpaulin # code coverage
+    cargo-watch
+  ];
+
+  cachix.enable = false;
+
+  enterShell = ''
+    echo "$GREET"
+  '';
+
+  languages.rust = {
+    enable = true;
+    mold.enable = true; # faster linking
+    channel = "stable";
+  };
+
+  pre-commit.hooks = {
+    clippy.enable = true;
+    clippy.settings.allFeatures = true;
+
+    treefmt.enable = true;
+    treefmt.settings.formatters = [
+      pkgs.nixfmt-rfc-style
+      pkgs.rustfmt
+    ];
+  };
+
+  processes = {
+    watch.exec = "cargo watch -x check -x test -x run";
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
