fcceed3db18a3fd9cf029db44e5a4e80743fa232
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-21 12:44:08 +0100

    feat: bind to random port when testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port.");
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on the Server
-    run()?.await
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,12 @@
-// use zero2prod::main;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -17,10 +16,13 @@ async fn health_check_works() {
 }
 
 // Run application in the background
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address.");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
+    tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
