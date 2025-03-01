37a5afe82fe52a756d6771e6711cd71b3610adca
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-01 16:41:41 +0100

    chore(nix): replace devenv with flake

---
+++ nix/modules/devShells.nix
@@ -0,0 +1,25 @@
+{
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      ...
+    }:
+    {
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [
+          self'.devShells.rust
+          self'.devShells.aliases
+          config.pre-commit.devShell
+          config.treefmt.build.devShell
+        ];
+
+        packages = with pkgs; [
+          cargo-auditable
+          cargo-tarpaulin # code coverage
+          cargo-udeps # unused deps
+        ];
+      };
+    };
+}
