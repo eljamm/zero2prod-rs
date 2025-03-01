a58a7b0813e3937688b46c8cd216e74131cb9ef0
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-01 16:53:22 +0100

    chore(nix): add yaml formatter

--- nix/modules/formatting.nix
+++ nix/modules/formatting.nix
@@ -12,8 +12,10 @@
         projectRootFile = "flake.nix";
         programs = {
           nixfmt.enable = true;
+          prettier.enable = true;
           rustfmt.enable = true;
           taplo.enable = true; # TOML
+          yamlfmt.enable = true;
         };
       };
 
