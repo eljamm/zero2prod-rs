90d66316dd2d3439e2bb54e13af9c135a174757c
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-08 13:32:39 +0100

    docs: remove outdated comment

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,7 +8,7 @@ fn spawn_app() -> String {
     let server = zero2prod::run(listener).expect("Failed to bind address.");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
+    // but we have no use for it here
     tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
