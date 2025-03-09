510cac6cf7742d3819308d0fef33f5195a2d09d4
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-09 17:07:10 +0100

    test: start a new logical database for each test
    This ensures that there is no conflict between tests and that the result
    is reproducible.

--- src/config.rs
+++ src/config.rs
@@ -22,6 +22,12 @@ impl DatabaseSettings {
             self.username, self.password, self.host, self.port, self.name
         )
     }
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
 }
 
 pub fn get_config() -> Result<Settings, config::ConfigError> {
