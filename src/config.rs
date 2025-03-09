7ceaf5a2d11ae373d6a642aac56e34b9e83144f0
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-09 11:52:20 +0100

    feat: init config file

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,24 @@
+use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub app_port: u16,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DatabaseSettings {
+    pub name: String,
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+}
+
+pub fn get_config() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new("config.yaml", config::FileFormat::Yaml))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
