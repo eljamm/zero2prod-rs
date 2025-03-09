255c6846aebb11d1d5704983ebce76c5739c67aa
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-09 16:36:55 +0100

    feat: replace PgConnection with PgPool
    https://docs.rs/sqlx/0.8.3/sqlx/struct.Pool.html

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::net::TcpListener;
 
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use zero2prod::config::get_config;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = get_config().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&config.database.connection_string())
+    let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
         .expect("Failed to connect to PostgreSQL.");
     let address = format!("127.0.0.1:{}", config.app_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
