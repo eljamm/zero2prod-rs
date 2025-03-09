a7b7ed723f5ee2fe31eff34e3d408a201f42693a
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-09 11:52:20 +0100

    feat: init config file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::net::TcpListener;
 
+use zero2prod::config::get_config;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port.");
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on the Server
+    let config = get_config().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", config.app_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
