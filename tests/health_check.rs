07e488e13b3b1e8901cc66728afe1815d165269a
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-09 12:30:52 +0100

    test: validate form

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -37,7 +37,7 @@ async fn subscribe_return_200_valid_form() {
     let config = get_config().expect("Failed to read configuration.");
 
     let connection_string = config.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to PostgreSQL.");
 
@@ -53,6 +53,14 @@ async fn subscribe_return_200_valid_form() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscriptions.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
